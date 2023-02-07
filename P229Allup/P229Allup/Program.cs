dbcaa875f665e7939632cbdd181c2c06ff7ef407
HamidMammadov <hamidvm@code.edu.az>
2023-02-07 17:23:29 +0400

    First Commit

---
+++ P229Allup/P229Allup/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using P229Allup.DataAccessLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
